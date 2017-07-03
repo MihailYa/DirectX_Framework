d433c87757456b9518a2a6e5a23d615daeace9ea
MihailYa <mikle3ddd@gmail.com>
2017-07-03 14:19:40 +0300

    Init

---
+++ DirectX Framework/DirectX Framework/stdafx.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <clocale>
+#include <ctime>
+
+#include <string>
+#include <list>
+
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+
+#include "macros.h"
+
+#include <d3d11.h>
+
+#pragma comment(lib, "d3d11.lib")
+
+#ifdef _DEBUG
+#	pragma comment(lib, "d3dx11d.lib")
+#else
+#	pragma comment(lib, "d3dx11.lib")
+#endif
